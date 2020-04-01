eabbadd41104b30cd3f04576b9ca0085a1955b04
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-01 19:34:38 +0530

    remove

--- kinesisnodejs.js
+++ kinesisnodejs.js
@@ -1,18 +1,4 @@
-let AWS = require('aws-sdk');
-const kinesis = new AWS.Kinesis();
-
 exports.handler = async (event) => {
-    try {
-        let data = await kinesis.putRecord({
-            StreamName: "K2",
-            Data: "kupdate",
-            PartitionKey: "m"
-        }).promise();
-        console.log(data);
-
-    } catch (err) {
-        // error handling goes here
-    };
-
+   
     return { "message": "Successfully executed" };
 };
