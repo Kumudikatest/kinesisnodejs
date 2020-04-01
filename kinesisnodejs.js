872087517b6349ae09b21c3681c8bd117ecc46f9
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-01 17:54:34 +0530

    remove resource

--- kinesisnodejs.js
+++ kinesisnodejs.js
@@ -1,17 +1,4 @@
-let AWS = require('aws-sdk');
-const kinesis = new AWS.Kinesis();
-
 exports.handler = async (event) => {
-    try {
-        let data = await kinesis.describeStream({
-            StreamName: "K2",
-            Limit: 2,
-            ExclusiveStartShardId: "shardId-000000000000"
-        }).promise();
-        console.log(data);
-    } catch (err) {
-        // error handling goes here
-    };
-
+  
     return { "message": "Successfully executed" };
 };
