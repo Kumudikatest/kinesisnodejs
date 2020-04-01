7e37b2fc50ad3628463809ebf9c5f41665635555
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-01 19:25:19 +0530

    test

--- kinesisnodejs.js
+++ kinesisnodejs.js
@@ -1,4 +1,18 @@
+let AWS = require('aws-sdk');
+const kinesis = new AWS.Kinesis();
+
 exports.handler = async (event) => {
-  
+    try {
+        let data = await kinesis.putRecord({
+            StreamName: "K2",
+            Data: "k",
+            PartitionKey: "m"
+        }).promise();
+        console.log(data);
+
+    } catch (err) {
+        // error handling goes here
+    };
+
     return { "message": "Successfully executed" };
 };
