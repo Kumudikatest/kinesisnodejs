e678b57202f462b31c36dd1f7ff5dd531cdbda6e
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-01 16:54:02 +0530

    update resource

--- kinesisnodejs.js
+++ kinesisnodejs.js
@@ -4,9 +4,11 @@ const kinesis = new AWS.Kinesis();
 exports.handler = async (event) => {
     try {
         let data = await kinesis.describeStream({
-            StreamName: "K2"
+            StreamName: "K2",
+            Limit: 2,
+            ExclusiveStartShardId: "shardId-000000000000"
         }).promise();
-console.log(data);
+        console.log(data);
     } catch (err) {
         // error handling goes here
     };
